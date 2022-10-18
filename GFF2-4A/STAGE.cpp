d8975959efaf29769b105acfc2ac9be60f1e6ae1
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2022-10-18 14:29:04 +0900

    背景画像追加

--- GFF2-4A/STAGE.cpp
+++ GFF2-4A/STAGE.cpp
@@ -6,20 +6,24 @@ float STAGE::scroll_x = 0;
 float STAGE::scroll_y = 0;
 
 STAGE::STAGE() {
-	**map_data = { 0 };
-	*image1 = { 0 };
-	LoadDivGraph("Resource/Images/Stage/StageBlock.png",8,8,1,80,80,image1);
+	**map_data = 0;
+	*block_image1 = 0;
+	*stage_image = 0;
+	stage_image[0] = LoadGraph("Resource/Images/Stage/BackImage.png");
+	LoadDivGraph("Resource/Images/Stage/StageBlock.png", 8, 8, 1, 80, 80, block_image1);
 	InitStage();
 }
 
 
-void STAGE::Update(float player_x,float player_y) {
+void STAGE::Update() {
 	//map_x = player_x - float((MAP_WIDTH/3 + 2) / 2 - 1);
 	//map_x++;
 	//map_y = player_y - float((MAP_HEIGHT + 2) / 2 - 1);
 }
 
 void STAGE::Draw()const {
+	DrawGraph(scroll_x, scroll_y, stage_image[0], FALSE);
+
 	for (int i = 0; i < MAP_HEIGHT; i++) {
 		for (int j = 0; j < MAP_WIDTH; j++) {
 			//DrawFormatString(200+j*30, 50+i*30, 0xffffff, "%3d ", map_data[i + int(map_x)][j + int(map_y)]);
@@ -28,10 +32,10 @@ void STAGE::Draw()const {
 				continue;*/
 
 			if (map_data[i ][j ] == 1) {
-				DrawGraph(j*MAP_CEllSIZE+scroll_x, i*MAP_CEllSIZE, image1[0], TRUE);
+				DrawGraph(j*MAP_CEllSIZE+scroll_x, i*MAP_CEllSIZE, block_image1[0], TRUE);
 			}
 			else if (map_data[i][j ] == 2) {
-				DrawGraph(j * MAP_CEllSIZE+scroll_x , i*MAP_CEllSIZE , image1[3], TRUE);
+				DrawGraph(j * MAP_CEllSIZE+scroll_x , i*MAP_CEllSIZE , block_image1[3], TRUE);
 			}
 			
 		}
