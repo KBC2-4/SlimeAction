bed418fc632b1c660c4dd4db65f90805bd303e89
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2022-10-24 14:27:50 +0900

    フックの座標取得関数追加

--- GFF2-4A/Element.cpp
+++ GFF2-4A/Element.cpp
@@ -1,2 +1,14 @@
 
 #include "Element.h"
+
+Element::Element() {
+	for (int i = 0; i < MAP_HEIGHT; i++)
+	{
+		for (int j = 0; j < MAP_WIDTH; j++)
+		{
+			ELEMENT_DATA data = { j * MAP_CEllSIZE, i * MAP_CEllSIZE };
+			if (map_data[i][j] == 70)hook.push_back(data);
+		}
+	}
+}
+
