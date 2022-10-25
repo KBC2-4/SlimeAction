eb158eccdbc58d887b4869c2c01564407c520816
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2022-10-25 12:11:18 +0900

    Elementクラスの名称を大文字に修正 ELEMENTクラスのメンバにvectorを追加

--- GFF2-4A/Element.cpp
+++ GFF2-4A/Element.cpp
@@ -1,13 +1,14 @@
 
 #include "Element.h"
 
-Element::Element() {
+ELEMENT::ELEMENT() {
 	for (int i = 0; i < MAP_HEIGHT; i++)
 	{
 		for (int j = 0; j < MAP_WIDTH; j++)
 		{
 			ELEMENT_DATA data = { j * MAP_CEllSIZE + MAP_CEllSIZE / 2, i * MAP_CEllSIZE + MAP_CEllSIZE / 2 };
 			if (map_data[i][j] == 70)hook.push_back(data);
+
 		}
 	}
 }
--- GFF2-4A/Element.h
+++ GFF2-4A/Element.h
@@ -2,23 +2,30 @@
 #include "STAGE.h"
 #include <vector>
 
-class Element :
+
+
+class ELEMENT :
     public STAGE
 {
 public:
 	struct ELEMENT_DATA
 	{
 		float x, y;
+		int type;
 	};
 private:
-	std::vector<ELEMENT_DATA> hook;	
-	ELEMENT_DATA button;
-	ELEMENT_DATA door;
-	ELEMENT_DATA lift;
-	ELEMENT_DATA tunnel;
+	std::vector<ELEMENT_DATA> hook;			//フック
+	std::vector<ELEMENT_DATA> button;		//ボタン
+	std::vector<ELEMENT_DATA> door;			//ドア
+	std::vector<ELEMENT_DATA> lift;			//動く床
+	std::vector<ELEMENT_DATA> tunnel;		//トンネル
 
 public:
-	Element();
+	ELEMENT();
 	std::vector<ELEMENT_DATA> GetHookPos() { return hook; }
+	std::vector<ELEMENT_DATA>GetButtonPos() { return button; }
+	std::vector<ELEMENT_DATA>GetDoorPos() { return door; }
+	std::vector<ELEMENT_DATA>GetLiftPos() { return lift; }
+	std::vector<ELEMENT_DATA>GetTunnel() { return tunnel; }
 };
 
