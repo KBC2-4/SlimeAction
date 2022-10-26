d93c3ef4aa76c54c6176dac106157db51ac2c25d
hikki1123 <106945420+hikki1123@users.noreply.github.com>
2022-10-26 11:54:05 +0900

    ちょっと

--- GFF2-4A/GrapeFruit.cpp
+++ GFF2-4A/GrapeFruit.cpp
@@ -53,5 +53,6 @@ void GRAPEFRUIT::Animation()
 void GRAPEFRUIT::Draw() const
 {
 	DrawRotaGraph2(640, 40, 40, 0, 2, sin(angle), image[0], TRUE);
+	DrawFormatString(50, 50, 0x00ff00, "%d", angle);
 	//DrawGraph(0, 0, image[0],TRUE);
 }
