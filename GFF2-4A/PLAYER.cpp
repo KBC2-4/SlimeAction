509c1f4a10d7f80261e6d3ce2563d03ad6bc95d6
manamu3 <manamu-shiroma-k21h@stu.kbc.ac.jp>
2022-11-22 08:56:29 +0900

    当たり判定の修正

--- GFF2-4A/PLAYER.cpp
+++ GFF2-4A/PLAYER.cpp
@@ -777,9 +777,9 @@ void PLAYER::HitBlock(ELEMENT* element,STAGE* stage) {
 					int block_type = stage->GetMapData(i, j);
 					if (!hit_ceil) {
 						//ドアの判定
-						if ((block_type == 66 || block_type == 67) && move_x > 0) {
-							return;
-						}
+						//if ((block_type == 66 || block_type == 67) && move_x > 0) {
+						//	//return;
+						//}
 						player_x = old_player_x;
 						if (hitBullet) {
 							player_x -= move_x * player_scale * SPEED * 2;
