5de1e2cd9f1a4f51f6c7a8a570ec771a048820fa
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2022-11-22 09:05:27 +0900

    Add "TimeAttack" Font

--- GFF2-4A/RESULT.cpp
+++ GFF2-4A/RESULT.cpp
@@ -40,7 +40,8 @@ RESULT::RESULT(bool issue, int clear_time) {
 
 	title_font = CreateFontToHandle("UD デジタル 教科書体 N-B", 140, 1, DX_FONTTYPE_ANTIALIASING_EDGE_8X8,-1,8);
 	menu_font = CreateFontToHandle("メイリオ", 100, 1, DX_FONTTYPE_ANTIALIASING_EDGE_8X8);
-	time_font = CreateFontToHandle("メイリオ", 90, 1, DX_FONTTYPE_ANTIALIASING_EDGE_8X8,-1,5);
+	AddFontResourceEx(TEXT("./Resource/Fonts/TimeAttack.otf"), FR_PRIVATE, NULL);
+	time_font = CreateFontToHandle("TimeAttack", 90, 1, DX_FONTTYPE_ANTIALIASING_EDGE_8X8,DX_CHARSET_DEFAULT,5);
 
 	if (issue == true) { timer = 10 * 60; }
 	else{ timer = 8 * 60; }
@@ -51,6 +52,7 @@ RESULT::RESULT(bool issue, int clear_time) {
 	se_randnum = GetRand(3);
 
 	*effect_timer = 0;
+	guide_timer = 0;
 }
 
 RESULT::~RESULT() {
@@ -59,6 +61,7 @@ RESULT::~RESULT() {
 	DeleteFontToHandle(title_font);
 	DeleteFontToHandle(menu_font);
 	DeleteFontToHandle(time_font);
+	RemoveFontResourceEx(TEXT("./Resource/Fonts/TimeAttack.otf"), FR_PRIVATE, NULL);
 	DeleteSoundMem(count_se);
 	DeleteSoundMem(ok_se);
 	for(int i = 0; i < 4; i++)DeleteSoundMem(good_se[i]);
@@ -77,6 +80,10 @@ AbstractScene* RESULT::Update() {
 	if (timer <= 5 * 60) { if (CheckSoundMem(count_se) == FALSE)PlaySoundMem(count_se, DX_PLAYTYPE_BACK, FALSE); }
 
 	if (--timer <= 60) { return new GAMEMAIN(); }
+
+	//ガイド点滅表示
+	if (guide_timer < 100) { guide_timer++; }
+	else { guide_timer = 0; }
 
 	if (PAD_INPUT::GetNowKey() == XINPUT_BUTTON_B && PAD_INPUT::GetPadState() == PAD_STATE::ON) { PlaySoundMem(ok_se, DX_PLAYTYPE_BACK, TRUE); return new GAMEMAIN(); }
 
@@ -98,9 +105,9 @@ void RESULT::Draw() const {
 		DrawStringToHandle(330, 350, "クリアタイム", 0xF5EB67, time_font, 0xFFFFFF);
 		char dis_clear_time[20];	//文字列合成バッファー
 		//文字列合成
-		if (clear_time / 1000 >= 60) { sprintf_s(dis_clear_time, sizeof(dis_clear_time), "%2d分%2d秒%.3dミリ秒", (clear_time / 1000) / 60, (clear_time / 1000) % 60, clear_time % 1000); }
-		else { sprintf_s(dis_clear_time, sizeof(dis_clear_time), "%5d秒%.3dミリ秒", clear_time / 1000, clear_time % 1000); }
-		DrawStringToHandle(160, 450, dis_clear_time, 0xF5EB67, time_font, 0xFFFFFF);	//クリアタイム
+		if (clear_time / 1000 >= 60) { sprintf_s(dis_clear_time, sizeof(dis_clear_time), "%4d:%2d.%.3d", (clear_time / 1000) / 60, (clear_time / 1000) % 60, clear_time % 1000); }
+		else { sprintf_s(dis_clear_time, sizeof(dis_clear_time), "%5d.%.3d", clear_time / 1000, clear_time % 1000); }
+		DrawStringToHandle(100, 400, dis_clear_time, 0xF5EB67, time_font, 0xFFFFFF);	//クリアタイム
 		DrawFormatStringToHandle(20, 560, 0x56F590, menu_font , "%2d秒後にリスタートします", timer / 60);
 	}
 	else {
@@ -118,12 +125,10 @@ void RESULT::Draw() const {
 		//DrawStringToHandle(90, 200, "ゲームオーバー", 0xEB8A95, title_font , 0xEB3D49);
 		DrawFormatStringToHandle(GetDrawCenterX(1280, "00秒後にリスタートします", menu_font), 400, 0x56F590, menu_font, "%2d秒後にリスタートします", timer / 60);
 	}
-
-	static int timer = 0;
-	if (timer++ < 50) {
+	//ガイド点滅表示
+	if (guide_timer < 50) {
 		DrawCircleAA(530.0f, 668.0f, 22, 20, 0xFFFFFF, 1);
 		DrawExtendStringToHandle(518, 650, 0.4f, 0.4f, "B", 0xEB7415, menu_font, 0xFFFFFF);
 		DrawExtendStringToHandle(560, 650, 0.4f, 0.4f, "でスキップ", 0x76F567, menu_font, 0xFFFFFF);
 	}
-	else if (timer > 100) { timer = 0; }
 }
