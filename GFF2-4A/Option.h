e3ae81bb9c638293ce175f4755b3dba615e1b8b8
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2022-12-11 22:06:19 +0900

    Add Option

---
+++ GFF2-4A/Option.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "AbstractScene.h"
+
+
+
+class Option :	public AbstractScene
+{
+private:
+    int menu_font;
+    int background_image;
+    int background_music;
+    int cursor_move_se;
+    int input_margin;
+    int bgm_vol;        //BGMの音量
+    int se_vol;         //SEの音量
+    int selectmenu;     //選択しているメニュー
+
+public:
+
+    //コンストラクタ
+    Option();
+    //デストラクタ
+    ~Option();
+    //描画以外の更新を実行
+    AbstractScene* Update() override;
+    //描画に関することを実装
+    void Draw() const override;
+
+    int GetBGMVolume(void) { return bgm_vol; }
+    int GetSEVolume(void) { return se_vol; }
+};
+
