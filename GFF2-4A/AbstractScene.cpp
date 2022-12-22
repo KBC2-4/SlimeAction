51e2cc2ae1b2b8ed0fdd97b200cc91380a665a3c
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2022-12-22 15:16:18 +0900

    Update GetDrawCenterX function

--- GFF2-4A/AbstractScene.cpp
+++ GFF2-4A/AbstractScene.cpp
@@ -1,11 +1,20 @@
 #include "AbstractScene.h"
 #include "DxLib.h"
 
-int AbstractScene::GetDrawCenterX(const char* string, int font_handle)const {
-	
+int AbstractScene::GetDrawCenterX(const char* string, int font_handle, int margin)const {
+
 	//‰æ–Ê•
 	const int screenX = 1280;
 
-	const int w = screenX / 2 - GetDrawFormatStringWidthToHandle(font_handle, string) / 2;
+	if (margin >= screenX || margin <= -screenX) {
+		margin = 0;
+	}
+
+	if (font_handle == 0) {
+		font_handle = DX_DEFAULT_FONT_HANDLE;
+	}
+
+
+	const int w = screenX / 2 + margin - (GetDrawFormatStringWidthToHandle(font_handle, string) / 2);
 	return w;
 }
--- GFF2-4A/AbstractScene.h
+++ GFF2-4A/AbstractScene.h
@@ -9,6 +9,11 @@ public:
 	virtual AbstractScene* Update() = 0;
 	//描画に関することを実装
 	virtual void Draw() const = 0;
-
-	int GetDrawCenterX(const char* string, int font_handle)const;
+	/// <summary>
+	/// 文字の描画するX座標が中心になるX座標を取得する。
+	/// </summary>
+	/// <param name="string">描画する文字列</param>
+	/// <param name="font_handle">描画に使用するフォントハンドル(未入力又は0の場合はデフォルトフォントハンドルを使用</param>
+	/// <param name="margin">中央のX座標に間隔を空ける値</param>
+	int GetDrawCenterX(const char* string, int font_handle = 0, int margin = 0)const;
 };
